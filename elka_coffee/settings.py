d6aea0c685629e277c668bf1942ced954c4a4bdc
mateuszmarzec <mateusz.marzec@jmr.pl>
2019-05-24 09:40:13 +0200

    Changes database to postgres.

--- elka_coffee/settings.py
+++ elka_coffee/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'elka_coffee.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'postgres',
+        'PORT': 5432,
     }
 }
 
