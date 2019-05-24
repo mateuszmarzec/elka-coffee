a339ff5336ded1892ead6b5e92e5202a938763c9
mateusz <mateuszmikolajmarzec@gmail.com>
2019-05-25 00:42:11 +0200

    Fix.

--- elka_coffee/settings/production.py
+++ elka_coffee/settings/production.py
@@ -6,4 +6,4 @@ ALLOWED_HOSTS = ['elka-coffee.herokuapp.com']
 
 DEBUG = False
 
-DATABASES['default'] = dj_database_url.config(default='postgres://fulpwaggtrzyzw:73210d861d1d572df5217ba67ddc686759e1f5e1cecca1ee06cfa7ce28992bf0@ec2-54-83-36-37.compute-1.amazonaws.com:5432/dftcuad261nv42')
+DATABASES['default'] = dj_database_url.config(default=os.environ.get('DATABASE_URL'))
