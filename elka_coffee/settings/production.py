50e7c789c2e2cdc89b9c8625373e4008ebc546a6
mateuszmarzec <mateusz.marzec@jmr.pl>
2019-06-04 10:12:46 +0200

    Fix

--- elka_coffee/settings/production.py
+++ elka_coffee/settings/production.py
@@ -4,6 +4,6 @@ from elka_coffee.settings.settings import *
 
 ALLOWED_HOSTS = ['elka-coffee.herokuapp.com']
 
-DEBUG = True
+DEBUG = False
 
 DATABASES['default'] = dj_database_url.config(default=os.environ.get('DATABASE_URL'))
